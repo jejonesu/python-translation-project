67e06de800a50860cf50e2ef6b60bf9350ce1d8e
Jennifer U. Jones <juj0001@auburn.edu>
2020-03-18 16:20:52 -0500

    finished project

--- translate.py
+++ translate.py
@@ -24,18 +24,36 @@ def translate_sequence(rna_sequence, genetic_code):
 
 def get_all_translations(rna_sequence, genetic_code):
     rna_seqs = rna_sequence.upper()
-    protein = []
-    if rna_seqs.count('AUG') == 0:
-        protein = []
-    if rna_seqs.count('AUG') == 1:
-        protein = ['M']
-    if rna_seqs.count('AUG') == 2:
-        rna_seqs = rna_seqs.split('AUG')
-        if rna_seqs.count('') == 1:
-            del rna_seqs[0]
-            for section in rna_seqs:
-                codon = rna_seqs[section:section + 3]
-                protein+= genetic_code[codon]
+    aa_list = []
+    start_pos = 0
+
+    def translate(start_pos,rna_seqs,genetic_code):
+        proteins = ''
+        for i in range(start_pos, len(rna_seqs), 3):
+            codon = rna_seqs[i:i + 3]
+            if codon in ['UAG', 'UAA', 'UGA'] or len(codon) != 3:
+                break
+            else: proteins += genetic_code[codon]
+        return proteins
+
+    while start_pos < len(rna_seqs):
+        start_codon = rna_seqs[start_pos:start_pos + 3]
+        if start_codon == 'AUG':
+            translation = translate(start_pos, rna_seqs, genetic_code)
+            aa_list.append(translation)
+        start_pos += 1
+    return aa_list
+#    if rna_seqs.count('AUG') == 0:
+#        protein = []
+#    if rna_seqs.count('AUG') == 1:
+#        protein = ['M']
+#    if rna_seqs.count('AUG') == 2:
+#        rna_seqs = rna_seqs.split('AUG')
+#        if rna_seqs.count('') == 1:
+#            del rna_seqs[0]
+#            for section in rna_seqs:
+#                codon = rna_seqs[section:section + 3]
+#                protein+= genetic_code[codon]
 
 
         #if rna_seqs.count('') == 0:
@@ -62,14 +80,15 @@ def get_all_translations(rna_sequence, genetic_code):
     #        protein = ['M'+name for name in protein(name) if e]
     #    for i in range(0,len(protein)):
     #        protein[i] = str(protein[i])
-    return protein
-        #if '*' in str(protein):
+    #return protein
+    pass
+ #if '*' in str(protein):
             #sep = '*'
             #protein = protein.split(sep, 1)[0]
             #protein =  protein.replace('*', '')
     #sep = 'M'
     #protein = protein.split(sep, 1)[0] 
-"""Get a list of all amino acid sequences encoded by an RNA sequence.
+    """Get a list of all amino acid sequences encoded by an RNA sequence.
 
     All three reading frames of `rna_sequence` are scanned from 'left' to
     'right', and the generation of a sequence of amino acids is started
@@ -99,14 +118,13 @@ def get_all_translations(rna_sequence, genetic_code):
         A list of strings; each string is an sequence of amino acids encoded by
         `rna_sequence`.
     """
-pass
 
 def get_reverse(sequence):
     seqs = list(sequence.upper())
     rev_seqs = seqs[::-1]
     reversed = ("".join(rev_seqs))
     return reversed
-    pass
+    pass #get reverse
 
 def get_complement(sequence):
     sequence = list(sequence.upper())
@@ -121,7 +139,7 @@ def get_complement(sequence):
         if i == "G":
             complement += "C"
     return complement
-    pass
+    pass #get complement
 
 def reverse_and_complement(sequence):
     seqs = list(sequence.upper())
@@ -138,10 +156,70 @@ def reverse_and_complement(sequence):
         if i == "G":
             complement += "C"
     return complement
-    pass
+    pass #reverse and complement
 
 def get_longest_peptide(rna_sequence, genetic_code):
-    """Get the longest peptide encoded by an RNA sequence.
+    rna_seqs = rna_sequence.upper()
+    aa_list = []
+    start_pos = 0
+
+    def translate(start_pos,rna_seqs,genetic_code):
+        proteins = ''
+        for i in range(start_pos, len(rna_seqs), 3):
+            codon = rna_seqs[i:i + 3]
+            if codon in ['UAG', 'UAA', 'UGA'] or len(codon) != 3:
+                break
+            else: proteins += genetic_code[codon]
+        return proteins
+
+    def reverse_and_complement(rna_seqs):
+        rna_seqs = list(rna_seqs.upper())
+        rev_seqs = rna_seqs[::-1]
+        reversed = ("".join(rev_seqs))
+        complement = ""
+        for i in reversed:
+            if i == "A":
+                complement += "U"
+            if i == "U":
+                complement += "A"
+            if i == "C":
+                complement += "G"
+            if i == "G":
+                complement += "C"
+        return complement
+
+    rev_and_comp = reverse_and_complement(rna_seqs)
+#    translation = translate(start_pos, rna_seqs, genetic_code)
+#   translation_back = translate(start_pos, rev_and_comp, genetic_code)
+
+    while start_pos < len(rna_seqs):
+        start_codon = rna_seqs[start_pos:start_pos + 3]
+        if start_codon == 'AUG':
+            translation = translate(start_pos, rna_seqs, genetic_code)
+            aa_list.append(translation)
+        start_codon = rev_and_comp[start_pos:start_pos + 3]
+        if start_codon == 'AUG':
+            translation_back = translate(start_pos, rev_and_comp, genetic_code)
+            aa_list.append(translation_back)
+        start_pos += 1
+
+#    while start_pos < len(rev_and_comp):
+#        start_codon = rev_and_comp[start_pos:start_pos + 3]
+#        if start_codon == 'AUG':
+#            translation_back = translate(start_pos, rev_and_comp, genetic_code)
+#            aa_list.append(translation_back)
+#        start_pos += 1
+
+    check = 'M'
+    aa_list_start =  [idx for idx in aa_list if idx[0].lower().startswith(check.lower())]
+    longest_aa = ''
+    if aa_list_start == []:
+        longest_aa == ''
+    if aa_list_start != []:
+        longest_aa = max((aa_list_start), key=len)
+    return longest_aa
+    pass
+"""Get the longest peptide encoded by an RNA sequence.
 
     Explore six reading frames of `rna_sequence` (the three reading frames of
     `rna_sequence`, and the three reading frames of the reverse and complement
@@ -167,7 +245,7 @@ def get_longest_peptide(rna_sequence, genetic_code):
         A string of the longest sequence of amino acids encoded by
         `rna_sequence`.
     """
-    pass
+
 
 
 if __name__ == '__main__':
