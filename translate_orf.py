d6c06f8743534fd0cba9946b7b111e698da438b9
Jennifer U. Jones <juj0001@auburn.edu>
2020-04-08 15:22:54 -0500

    part two finalized

--- translate_orf.py
+++ translate_orf.py
@@ -1,17 +1,13 @@
 #! /usr/bin/env python3
 
-from find_orf import find_first_orf
-
-from translate import translate_sequence
-
-from find_orf import parse_sequence_from_path
+import re
+import find_orf
+import sys
+import translate
+import argparse
 
 def main():
-    import argparse
-
-    # Create a command-line parser object
-    parser = argparse.ArgumentParser(
-            formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    orf_parse = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
     # Tell the parser what command-line arguments this script can receive
     parser.add_argument('sequence',
@@ -47,47 +43,17 @@ def main():
     else:
         sequence = args.sequence
 
-    orf = find_first_orf(sequence = sequence,
+    orf = find_orf.find_first_orf(sequence = sequence,
             start_codons = args.start_codons,
             stop_codons = args.stop_codons)
-    sys.stdout.write('{}\n'.format(orf))
 
-if __name__ == '__main__':
-    first_orf = find_first_orf(sequence, start_codons, stop_codons)
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L','GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
-    rna_seq = ("AUG"
-            "UAC"
-            "UGG"
-            "CAC"
-            "GCU"
-            "ACU"
-            "GCU"
-            "CCA"
-            "UAU"
-            "ACU"
-            "CAC"
-            "CAG"
-            "AAU"
-            "AUC"
-            "AGU"
-            "ACA"
-            "GCG")
-    translation = translate_sequence(first_orf, genetic_code)
-    assert isinstance(longest_peptide, str), "Oops: the longest peptide is {0}, not a string".format(longest_peptide)
-    message = "The longest peptide encoded by\n\t'{0}'\nis\n\t'{1}'\n".format(
-            rna_seq,
-            longest_peptide)
-    sys.stdout.write(message)
-    if longest_peptide == "MYWHATAPYTHQNISTA":
-        sys.stdout.write("Indeed.\n")
-        translation = translate_sequence(sequence)
-        sys.stdout.write(translation)
-    else:
-        sequence = args.sequence
 
-    orf = find_first_orf(sequence = sequence,
-            start_codons = args.start_codons,
-            stop_codons = args.stop_codons)
-    sys.stdout.write('{}\n'.format(orf))
 
+    translation = translate.translate_sequence(rna_sequence = orf, genetic_code = genetic_code)
+
+    sys.stdout.write('{}\n'.format(translating_time))
+
+    if __name__ == '__main__':
+        main()
 
